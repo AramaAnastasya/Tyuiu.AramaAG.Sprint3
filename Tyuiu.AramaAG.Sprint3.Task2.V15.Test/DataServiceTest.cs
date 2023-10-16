e14ebc2d298f7e0f841babf64330aa728fde1882
AramaAnastasya <Tyuiu.AramaAG@outlook.com>
2023-10-16 18:12:38 +0500

    Task2

---
+++ Tyuiu.AramaAG.Sprint3.Task2.V15.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.AramaAG.Sprint3.Task2.V15.Lib;
+
+namespace Tyuiu.AramaAG.Sprint3.Task2.V15.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumSeries()
+        {
+            DataService ds = new DataService();
+            double value = 0.5;
+            int startValue = 1;
+            int stopValue = 20;
+            double p = ds.GetSumSeries(value, startValue, stopValue);
+            Assert.AreEqual(0.298, p);
+        }
+    }
+}
