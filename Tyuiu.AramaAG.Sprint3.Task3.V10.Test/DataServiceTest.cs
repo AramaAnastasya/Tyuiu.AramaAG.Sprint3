572c7f48f937ee60220ff34ecb3096391174780c
AramaAnastasya <Tyuiu.AramaAG@outlook.com>
2023-10-17 18:49:15 +0500

    Task3

---
+++ Tyuiu.AramaAG.Sprint3.Task3.V10.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.AramaAG.Sprint3.Task3.V10.Lib;
+
+namespace Tyuiu.AramaAG.Sprint3.Task3.V10.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidDeleteCharInString()
+        {
+            DataService ds = new DataService();
+            string value = "gdfppf vfppt p";
+            char item = 'p';
+            string res = ds.DeleteCharInString(value, item);
+            Assert.AreEqual("gdff vft ", res);
+        }
+    }
+}
---
+++ Tyuiu.AramaAG.Sprint3.Task3.V10/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.AramaAG.Sprint3.Task3.V10.Lib;
+
+namespace Tyuiu.AramaAG.Sprint3.Task3.V10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Арама А.Г. | СМАРТб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Цикл foreach                                                      *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #10                                                             *");
+            Console.WriteLine("* Выполнил: Арама Анастасия Георгиевна | СМАРТб-23-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Используя цикл foreach удалить из строки все буквы p в строке:          *");
+            Console.WriteLine("* gdfppf vfppt p                                                          *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            string value = "gdfppf vfppt p";
+            char item = 'p';
+            Console.WriteLine($" Строка = {value}.\n Буква = {item}.                                      ");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            string res = ds.DeleteCharInString(value, item);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
