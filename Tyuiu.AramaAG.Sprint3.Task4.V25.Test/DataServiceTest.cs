d9547aecd0d90d4519cae8255e7b0abc87d773ce
AramaAnastasya <Tyuiu.AramaAG@outlook.com>
2023-10-26 11:55:10 +0500

    Task4

---
+++ Tyuiu.AramaAG.Sprint3.Task4.V25.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.AramaAG.Sprint3.Task4.V25.Lib;
+namespace Tyuiu.AramaAG.Sprint3.Task4.V25.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidDeleteCharInString()
+        {
+            DataService ds = new DataService();
+            int startValue = -5;
+            int stopValue = 5;
+            double res = ds.Calculate( startValue, stopValue);
+            Assert.AreEqual(44.31, res);
+        }
+    }
+}
